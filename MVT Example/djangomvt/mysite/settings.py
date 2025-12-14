50397599d13f267a1e8d2faa5812ead6e29c5708
novakvova <novakvova@gmail.com>
2025-12-14 15:15:01 +0200

    Working register new user

--- MVT Example/djangomvt/mysite/settings.py
+++ MVT Example/djangomvt/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'categories',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -138,6 +139,8 @@ STATICFILES_DIRS = [
 IMAGE_URL = '/images/'
 
 IMAGES_ROOT = BASE_DIR / 'images'
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- MVT Example/djangomvt/mysite/urls.py
+++ MVT Example/djangomvt/mysite/urls.py
@@ -22,6 +22,7 @@ from . import views
 
 urlpatterns = [
     path('categories/', include('categories.urls')),
+    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
     path('', views.homepage, name="homepage")
 ]
