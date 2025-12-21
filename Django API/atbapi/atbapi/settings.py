4820004fbdfd146d85df823fc48328463f32c162
novakvova <novakvova@gmail.com>
2025-12-21 17:43:24 +0200

    Is good working Swagger Django

--- Django API/atbapi/atbapi/settings.py
+++ Django API/atbapi/atbapi/settings.py
@@ -150,5 +150,6 @@ SPECTACULAR_SETTINGS = {
     'DESCRIPTION': 'З наступаючим Різдвом та Новим Роком',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
+    'COMPONENT_SPLIT_REQUEST': True,
     # OTHER SETTINGS
 }
